dbb7e3608bfa47f5ffb6bb3b5381e38bb6aa98be
Hexagonist <januszkrem2@wp.pl>
2025-04-10 02:24:54 +0200

    Added new pin_screen widget

--- nbank/lib/login_screens/pin_screen.dart
+++ nbank/lib/login_screens/pin_screen.dart
@@ -1,94 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../navigation/app_routes.dart';
-import 'login_screen.dart';
-
 
 class PinCodeWidget extends StatefulWidget {
   const PinCodeWidget({super.key});
 
   @override
-  // _PinLockScreenState createState() => _PinLockScreenState();
   State<PinCodeWidget> createState() => _PinCodeWidgetState();
 }
-
-// class _PinLockScreenState extends State<PinLockScreen> {
-//   final TextEditingController _pinController = TextEditingController();
-
-//   void _checkPin() {
-//     if (loggedInUser == null) {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(content: Text("Brak zalogowanego użytkownika")),
-//       );
-//       Navigator.pushReplacementNamed(context, AppRoutes.login);
-//       return;
-//     }
-
-//     if (_pinController.text == loggedInUser!.pin) {
-//       Navigator.pushReplacementNamed(context, AppRoutes.home);
-//     } else {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(content: Text("Niepoprawny PIN")),
-//       );
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text("Wprowadź PIN")),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text("Aplikacja została zablokowana po bezczynności."),
-//             SizedBox(height: 12),
-//             TextField(
-//               controller: _pinController,
-//               decoration: InputDecoration(labelText: "PIN"),
-//               obscureText: true,
-//               keyboardType: TextInputType.number,
-//             ),
-//             SizedBox(height: 24),
-//             ElevatedButton(
-//               onPressed: _checkPin,
-//               child: Text("Odblokuj"),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-
-
-
 
 class _PinCodeWidgetState extends State<PinCodeWidget> {
   String enteredPin = '';
   bool isPinVisible = false;
-
-
-  void _checkPin() {
-    if (loggedInUser == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Brak zalogowanego użytkownika")),
-      );
-      Navigator.pushReplacementNamed(context, AppRoutes.login);
-      return;
-    }
-
-    if (enteredPin == loggedInUser!.pin) {
-      Navigator.pushReplacementNamed(context, AppRoutes.home);
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Niepoprawny PIN")),
-      );
-    }
-  }
 
   /// this widget will be use for each digit
   Widget numButton(int number) {
@@ -241,11 +163,6 @@ class _PinCodeWidgetState extends State<PinCodeWidget> {
                   color: Colors.black,
                 ),
               ),
-            ),
-
-            ElevatedButton(
-              onPressed: _checkPin,
-              child: Text("Odblokuj"),
             ),
           ],
         ),
