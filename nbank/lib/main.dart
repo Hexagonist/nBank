b474505c0128b0e99cec13ae2edd297bc8edce73
Hexagonist <januszkrem2@wp.pl>
2025-06-05 05:31:13 +0200

    App appearance upgrade

--- nbank/lib/main.dart
+++ nbank/lib/main.dart
@@ -3,14 +3,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart'; 
 import 'navigation/app_routes.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import '../theme/app_theme.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
   await initializeDateFormatting('pl', null);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,9 +21,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: AppTheme.lightTheme, // poprawka: użyj AppTheme.lightTheme
+      darkTheme: AppTheme.darkTheme, // opcjonalnie, jeśli chcesz obsługę dark mode
       initialRoute: AppRoutes.login,
       routes: AppRoutes.getRoutes(),
     );
--- nbank/lib/theme/app_theme.dart
+++ nbank/lib/theme/app_theme.dart
@@ -2,14 +2,30 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static final lightTheme = ThemeData(
-    brightness: Brightness.light,
-    primarySwatch: Colors.indigo,
+    useMaterial3: true, // zalecane dla nowych projektów
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
     scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.indigo,
+      foregroundColor: Colors.white,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.indigo,
+      foregroundColor: Colors.white,
+    ),
   );
 
   static final darkTheme = ThemeData(
-    brightness: Brightness.dark,
-    primarySwatch: Colors.indigo,
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo, brightness: Brightness.dark),
     scaffoldBackgroundColor: Colors.black,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.indigo,
+      foregroundColor: Colors.white,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.indigo,
+      foregroundColor: Colors.white,
+    ),
   );
 }
